c5cfefd091691917ae249acee3963f171ee3c616
Courtney Brothers <courtneywbrothers@gmail.com>
2018-03-01 15:55:55 -0600

    american flag

---
+++ flag.js
@@ -0,0 +1,21 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Hello world!'));
+
+const log = console.log;
+ 
+
+log(chalk.white.bgBlue.bold(` * * * * * * *  `),chalk.bgRed.bold('                              '));
+log(chalk.white.bgBlue.bold(`  * * * * * *   `),chalk.white.bold('                              '));
+log(chalk.white.bgBlue.bold(` * * * * * * *  `),chalk.bgRed.bold('                              '));
+log(chalk.white.bgBlue.bold(`  * * * * * *   `),chalk.white.bold('                              '));
+log(chalk.white.bgBlue.bold(` * * * * * * *  `),chalk.bgRed.bold('                              '));
+log(chalk.white. bgBlue.bold(`  * * * * * *   `),chalk.white.bold('                              '));
+log(chalk.white.bgBlue.bold(` * * * * * * *  `),chalk.bgRed.bold('                              '));
+log(chalk.white.bold('                                               '));
+log(chalk.bgRed.bold('                                               '));
+log(chalk.white.bold('                                               '));
+log(chalk.bgRed.bold('                                               '));
+log(chalk.white.bold('                                               '));
+log(chalk.bgRed.bold('                                               '));
+
