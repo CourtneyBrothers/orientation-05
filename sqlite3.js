30d6af49e5ece4f1d1ff27fe173ba90156bac034
Courtney Brothers <courtneywbrothers@gmail.com>
2018-03-05 11:55:43 -0600

    add drop to sqlite3 exercise

--- sqlite3.js
+++ sqlite3.js
@@ -5,6 +5,8 @@ const { Database } = require('sqlite3').verbose();
 // Database method accepts a callback function for successful connection
 const db = new Database('example.sqlite', () => console.log('Connected!'));
 
+ 
+
 db.run("CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT, jobTitle TEXT, address TEXT)");
 
 let employeeArray = [
@@ -14,14 +16,22 @@ let employeeArray = [
     { id: 3, firstName: 'kevin', lastName: 'Sm', jobTitle: 'ier', address: '400 Somewhere Lane' },
     { id: 4, firstName: 'Fran', lastName: 'mith', jobTitle: 'shier', address: '500 Somewhere Lane' },
     { id: 5, firstName: 'DLK', lastName: 'ith', jobTitle: 'ashier', address: '600 Somewhere Lane' }
-//   ]
+   ]
 
 
   //1.
   employeeArray.forEach((obj) => {
-    Using ES6 string templating, we can create an insert statement for each object
+    
     db.run(`INSERT INTO employees VALUES (${obj.id}, '${obj.firstName}', '${obj.lastName}','${obj.jobTitle}','${obj.address}')`);
   });
+
+
+//with destructuring
+ // employeeArray.forEach(({id},{firstName},{lastName},{jobTitle},{address}) => {
+    
+    //db.run(`INSERT INTO employees VALUES (${id}, '${firstName}', '${lastName}','${jobTitle}','${address}')`);
+  //});
+
 
 //2. Write a statement to query the database and console.log() all employee records.
 db.all("SELECT * FROM employees", (err, allRows) => {
@@ -46,3 +56,5 @@ db.all("SELECT * FROM employees", (err, allRows) => {
       console.log(each.first + ' ' + each.last, each.jobTitle);
     });
   });
+
+  //db.all db.each 
